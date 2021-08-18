07404606b9b3e3a2b6625ca5b97b227f8b2f79fe
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-18 18:49:32 +0530

    Added: UC3

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -3,14 +3,37 @@ import java.lang.Math;
 import java.util.Random;
 
 public class SnakeLadder {
+	public static final int No_Play = 1;
+	public static final int Ladder = 2;
+	public static final int Snake = 3;
+	
 	public static void main(String[] args) {
 	System.out.println("Welcome to Snake & Ladder");
 	int pos = 0;
 	Random rand = new Random();
 	
-	while(pos!=100) {
-		int dice = rand.nextInt(7) + 1;
+	while(pos<=100) {
+		
+		//Dice gives 1-6 random output
+		int dice = rand.nextInt(6) + 1;
 		System.out.println("Dice Number: "+dice);
+		
+		//Select random option from 1-3
+		int option = rand.nextInt(3) + 1;
+		switch(option) {
+		case No_Play: 
+			System.out.println("NO PLAY"); 
+			break;
+		case Ladder: 
+			pos=pos+dice; 
+			System.out.println("LADDER! Move ahead by "+dice+" steps. Final position :" +pos );
+			break;
+		case Snake:
+			pos=pos-dice;
+			System.out.println("SNAKE! Move back by "+dice+" steps. Final position :" +pos );
+			break;
+		}
+		
 	}
 	}
 }
