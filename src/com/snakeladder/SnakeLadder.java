61afa8b070aac501d1e61a7276b2d70ca81c6379
tusharkasturi1999 <tusharkasturi1999@gmail.com>
2021-08-18 17:31:05 +0530

    Added: UC2

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -1,8 +1,16 @@
 package com.snakeladder;
+import java.lang.Math;
+import java.util.Random;
 
 public class SnakeLadder {
 	public static void main(String[] args) {
 	System.out.println("Welcome to Snake & Ladder");
 	int pos = 0;
+	Random rand = new Random();
+	
+	while(pos!=100) {
+		int dice = rand.nextInt(7) + 1;
+		System.out.println("Dice Number: "+dice);
+	}
 	}
 }
